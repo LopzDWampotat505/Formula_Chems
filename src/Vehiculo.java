2c6c1f5c09c6c48fcd577a2aa57b2a343abac420
Juan Pablo Lopez Moreno <juan.lopezmo@udlap.mx>
2026-06-15 20:41:48 -0600

    Mejora y actualización de Escuderia y Vehiculo
    Agregada la logica para listas de Escuderia segun el reglamento de la FIA, se añadieron los atributos y metodos basicos para vehiculo (posteriormente agregar factor)

--- src/Vehiculo.java
+++ src/Vehiculo.java
@@ -1,2 +1,41 @@
 public class Vehiculo {
+    private byte motor;
+    private byte llantas;
+    private byte peso;
+
+    public Vehiculo(byte mo, byte llan, byte pes){
+        setMotor(mo);
+        setLlantas(llan);
+        setPeso(pes);
+    }
+
+    public byte getMotor(){
+        return motor;
+    }
+    public byte getLlantas(){
+        return llantas;
+    }
+    public byte getPeso(){
+        return peso;
+    }
+    public void setMotor(byte mo) {
+        if (mo < 0 || mo > 10) {
+            throw new IllegalArgumentException("La caracteristica debe estar entre 0 y 10. Valor intentado: " + mo);
+        }
+        this.motor = mo;
+    }
+
+    public void setLlantas(byte llan) { [cite: 142]
+        if (llan < 0 || llan > 10) {
+            throw new IllegalArgumentException("La caracteristica debe estar entre 0 y 10. Valor intentado: " + llan);
+        }
+        this.llantas = llan; [cite: 142]
+    }
+
+    public void setPeso(byte pes) {
+        if (pes < 0 || pes > 10) {
+            throw new IllegalArgumentException("La caracteristica de peso debe estar entre 0 y 10. Valor intentado: " + pes);
+        }
+        this.peso = pes;
+    }
 }
